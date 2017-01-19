03186d9e4e49f3ac8e9b538c258ba79afb7e49be
Hsue66 <dufmaakedl@gmail.com>
2017-01-19 16:52:12 +0900

    make Simple layout

--- app/components/main/main.style.js
+++ app/components/main/main.style.js
@@ -25,19 +25,61 @@ export default StyleSheet.create({
         backgroundColor: '#F5F5F5',
         flex: 1
     },
+
     frontpage: {
         width: width,
         height: height,
-        backgroundColor: '#FF5555',
+  //      backgroundColor: '#FF5555',
     },
+    locinfobar:{
+      margin: 20,
+    },
+    actionspace: {
+        height: height/3,
+        backgroundColor: '#FFFF55',
+        alignItems: 'center',
+        justifyContent:'center',
+    },
+
     underpage: {
+        flex:1,
         width: width,
         height: height,
-        backgroundColor: '#55FF55',
+      //  backgroundColor: '#55FF55',
     },
+    threedays: {
+        flex:0.5,
+        alignItems: 'center',
+        justifyContent:'center',
+        backgroundColor: '#888888',
+    },
+    alram: {
+        flex:0.5,
+        alignItems: 'center',
+        justifyContent:'center',
+        backgroundColor: '#F4FFF4',
+    },
+
+
     infopage: {
+        flex:1,
         width: width,
         height: height,
-        backgroundColor: '#5555FF',
+    //    backgroundColor: '#5555FF',
+    },
+    textsection: {
+        flex:0.07,
+        alignItems: 'center',
+        justifyContent:'center',
+    },
+    barsection:{
+      flex:0.23,
+      margin:20
+    },
+    detailsection:{
+      flex:0.7,
+      alignItems: 'center',
+      justifyContent:'center',
+      backgroundColor: '#AAAAAA',
     }
 });
